9c05f3ecec6e4d7354dafca58d902a4084fe80e7
Dylan Kok <tenkotofu@gmail.com>
2021-10-29 16:40:49 +0800

    Added logic to show password status

---
+++ SITConnect/Pages/PasswordFunc.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SITConnect.Pages
+{
+    public class PasswordFunc
+    {
+        public string GetPasswordStrength(string password)
+        {
+            // Ensure that password is not null
+            if (password == null)
+                // If it is, return error message to view
+                return "Please enter a password";
+
+            var score = 0;
+
+            if (password.Length >= 8) score++;
+
+            if (Regex.IsMatch(password, "[a-z]")) score++;
+
+            if (Regex.IsMatch(password, "[A-Z]")) score++;
+
+            if (Regex.IsMatch(password, "[0-9]")) score++;
+
+            if (Regex.IsMatch(password, "[^a-zA-Z0-9]+")) score++;
+
+            var status = score switch
+            {
+                1 => "Very Weak",
+                2 => "Weak",
+                3 => "Medium",
+                4 => "Strong",
+                5 => "Excellent",
+                _ => null
+            };
+
+            // Exit point
+            return status;
+        }
+    }
+}
---
+++ SITConnect/Pages/Register.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SITConnect.Pages
+{
+    public class Register : PageModel
+    {
+        // Import PasswordFunc
+        private readonly PasswordFunc _pwdFunc = new();
+
+        [BindProperty] public string CustFname { get; set; }
+        [BindProperty] public string CustLname { get; set; }
+        [BindProperty] public string BillingCardno { get; set; }
+        [BindProperty] public string CustEmail { get; set; }
+        [BindProperty] public string CustPwd { get; set; }
+        [BindProperty] public string CustDob { get; set; }
+        [BindProperty] public string CustPhoto { get; set; }
+
+        public string CustPasswordStatus { get; set; }
+
+        public void OnGet()
+        {
+            // Empty
+        }
+
+        public IActionResult OnPost()
+        {
+            CustPasswordStatus = _pwdFunc.GetPasswordStrength(CustPwd);
+            return Page();
+        }
+    }
+}
