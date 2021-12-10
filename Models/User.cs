17241c3e62c7a824e0a0eea2cad2a39de110e6ad
Dylan Kok <me@dylankjy.com>
2021-12-10 15:46:24 +0800

    Encryption/decryption for Card number

--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using IdGen;
+using SITConnect.Pages;
 
 namespace SITConnect.Models
 {
     public class User
     {
-        // All objects will have an ID, even if they exist in the database
-        // This ID is replaced with the actual stored once RetrieveData() is called.
-        // [DatabaseGenerated(DatabaseGeneratedOption.None), Key]
-        // public virtual long Id { get; set;} = new IdGenerator(0).CreateId();
+        // ID uses Twitter snowflake instead of incremental
         public long Id { get; set; } = new IdGenerator(0).CreateId();
 
         [Required] public string FirstName { get; set; }
@@ -31,22 +35,97 @@ namespace SITConnect.Models
             Password = hash;
         }
 
-        public void SetCardNo(string plainText)
-        {
-            // TODO: Encrypt card number before storing information
-            BillingCardNo = plainText;
-        }
-
-        public string GetCardNo()
-        {
-            // TODO Decryption
-            return BillingCardNo;
-        }
-
         public bool ComparePassword(string incoming)
         {
             var isPasswordMatching = BCrypt.Net.BCrypt.Verify(incoming, Password);
             return isPasswordMatching;
+        }
+        
+        public void SetCardNo(string plainText)
+        {
+            RandomStringGen randString = new RandomStringGen();
+            
+            // Use random generator to generate password (512 characters)
+            var encryptionKey = randString.RandomString();
+            
+            // Take password and encrypt the card no.
+            var encryptionResult = Encrypt(plainText, encryptionKey);
+
+            // Set the cipher text as the prop
+            BillingCardNo = encryptionResult.Item2;
+            
+            // Write the User ID, Initialisation Vector and encryption key to txt file stored in /CryptoStore/keys.csv
+            File.AppendAllText(Environment.CurrentDirectory + @"\CryptoStore\keys.csv", $"{Id},{encryptionResult.Item1},{encryptionResult.Item3}" + Environment.NewLine);
+        }
+
+        public dynamic GetCardNo()
+        {
+            // Read key file and get line by user id. Retrieve IV and key
+            string[] lineInKeyFile = File.ReadAllLines(Environment.CurrentDirectory + @"\CryptoStore\keys.csv")
+                .Where(s => s.Contains(Id.ToString())).ToArray();
+
+            // Assume the first entry, since it would be the only one
+            string[] elements = lineInKeyFile[0].Split(",");
+
+            string iv = elements[1];
+            string key = elements[2];
+            Console.WriteLine(iv);
+            Console.WriteLine(key);
+
+            string decryptedCardNo = Decrypt(BillingCardNo, iv, key);
+
+            return decryptedCardNo;
+        }
+        
+        private static (string, string, string) Encrypt(string content, string password) // returns (base64Iv, base64Ciphertext, base64Key)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(content);
+            
+            using (SymmetricAlgorithm crypt = Aes.Create())
+            using (HashAlgorithm hash = SHA256.Create())
+            using (MemoryStream memoryStream = new MemoryStream()) // This initialises a new memoryStream to store blobs later
+            {
+                // Converts the password into SHA512 hash before using it for encryption
+                crypt.Key = hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                
+                // Generate an Initialisation Vector
+                crypt.GenerateIV();
+            
+                // Populate the memoryStream with the blob
+                using (CryptoStream cryptoStream = new CryptoStream(
+                    memoryStream, crypt.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(bytes, 0, bytes.Length);
+                }
+            
+                // Convert the initialisation vector to base64
+                string base64Iv = Convert.ToBase64String(crypt.IV);
+                string base64Ciphertext = Convert.ToBase64String(memoryStream.ToArray());
+                string base64Key = Convert.ToBase64String(crypt.Key);
+            
+                return (base64Iv, base64Ciphertext, base64Key);
+            }
+        }
+
+        private static string Decrypt(string cipherText, string iv, string key)
+        {
+            byte[] bytes = Convert.FromBase64String(cipherText);
+            byte[] ivBytes = Convert.FromBase64String(iv);
+            byte[] keyBytes = Convert.FromBase64String(key);
+
+            using (SymmetricAlgorithm crypt = Aes.Create())
+            {
+                crypt.IV = ivBytes;
+                crypt.Key = keyBytes;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = crypt.CreateDecryptor(crypt.Key, crypt.IV);
+                
+                byte[] decryptedText = decryptor.TransformFinalBlock(bytes, 0, bytes.Length);
+
+                // Convert back to string and return plain text
+                return Encoding.UTF8.GetString(decryptedText);
+            }
         }
     }
 }
---
+++ Pages/RandomStringGen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SITConnect.Pages
+{
+    public class RandomStringGen
+    {
+        private static readonly Random Random = new Random(); 
+        
+        public string RandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string randomString = "";
+
+            for (int i = 0; i < 512; i++)
+            {
+                randomString += chars[Random.Next(0, chars.Length - 1)];
+            }
+
+            return randomString;
+        }
+    }
+}
