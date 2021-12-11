390917d058a68fed2969dd08da6f147363176c18
Dylan Kok <me@dylankjy.com>
2021-12-11 18:35:17 +0800

    Added audit logging

---
+++ Models/AuditLogDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace SITConnect.Models
+{
+    public class AuditLogDbContext : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public AuditLogDbContext(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public DbSet<AuditLog> Audits { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _config.GetConnectionString("MyConn");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ Services/AuditLogService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SITConnect.Models;
+
+namespace SITConnect.Services
+{
+    public class AuditLogService
+    {
+        private readonly AuditLogDbContext _context;
+
+        public AuditLogService(AuditLogDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddLog(AuditLog userObject)
+        {
+            // if (_context.Audits.Any(o => o.audit == userObject.Id)) return false;
+            _context.Audits.Add(userObject);
+            _context.SaveChanges();
+        }
+
+        public IQueryable<AuditLog> GetLogsByUserId(long userId)
+        {
+            var listOfLogs = _context.Audits.Where(o => o.ActorId == userId);
+            return listOfLogs;
+            // Reminder to self: .ToList() if it needs to be read. This returns an IQueryable to allow Linq to work.
+        }
+    }
+}
