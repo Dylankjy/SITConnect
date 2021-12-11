3a1d48cc1f700cd02ab10218cae86237682571cd
Dylan Kok <me@dylankjy.com>
2021-12-11 21:29:30 +0800

    Added frontend to display audits

---
+++ Pages/AuditDisplay.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SITConnect.Models;
+using SITConnect.Services;
+
+namespace SITConnect.Pages
+{
+    public class AuditDisplay : PageModel
+    {
+        private readonly AuditLogService _auditDb;
+        private readonly UserService _userDb;
+
+        public AuditDisplay(UserService userService, AuditLogService auditService)
+        {
+            _userDb = userService;
+            _auditDb = auditService;
+        }
+
+        public List<AuditLog> AuditLogsToDisplay { get; set; }
+        public User CurrentUser { get; set; }
+        
+        public IActionResult OnGet()
+        {
+            if (!ModelState.IsValid && HttpContext.Session.GetString("user") == null)
+            {
+                return RedirectToPage("/Error403");
+            }
+            
+            CurrentUser = new User().FromJson(HttpContext.Session.GetString("user"));
+            
+            AuditLogsToDisplay = _auditDb.GetLogsByUserId(CurrentUser.Id).AsEnumerable().Reverse().ToList();
+            return Page();
+        }
+    }
+}
