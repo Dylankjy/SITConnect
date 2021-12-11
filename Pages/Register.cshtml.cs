974b69b1f6f0d44fd2aefe9f64371f5e4b730d54
Dylan Kok <me@dylankjy.com>
2021-12-11 18:39:43 +0800

    Email validation checks

--- Pages/Register.cshtml.cs
+++ Pages/Register.cshtml.cs
@@ -25,6 +25,7 @@ namespace SITConnect.Pages
         [BindProperty] public string BillingCardNo { get; set; }
 
         public string CustPasswordStatus { get; set; }
+        public string EmailStatus { get; set; }
         public string CardStatus { get; set; }
 
         public void OnGet()
@@ -57,6 +58,12 @@ namespace SITConnect.Pages
             Console.WriteLine(NewUser.LastName);
             Console.WriteLine(NewUser.DateOfBirth);
             Console.WriteLine(NewUser.GetCardNo());
+            // Check for existing user accounts with the same email address
+            if (_userDb.GetUserByEmail(NewUser.Email) != null)
+            {
+                EmailStatus = "This email is already in use.";
+                return Page();
+            }
 
             // Create user account
             _userDb.AddUser(NewUser);
