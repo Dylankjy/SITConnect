2980ea7dbd73de6ac3857ad855bee1a1a48569b6
Dylan Kok <me@dylankjy.com>
2021-12-20 04:05:04 +0800

    Add to audit on logout

--- Pages/Logout.cshtml.cs
+++ Pages/Logout.cshtml.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SITConnect.Models;
+using SITConnect.Services;
 
 namespace SITConnect.Pages
 {
     public class Logout : PageModel
     {
+        private readonly AuditLogService _auditDb;
+        private readonly UserService _userDb;
+
+        public Logout(UserService userService, AuditLogService auditService)
+        {
+            _userDb = userService;
+            _auditDb = auditService;
+        }
+
         public void OnGet()
         {
             // Empty
@@ -12,8 +24,17 @@ namespace SITConnect.Pages
 
         public IActionResult OnPost()
         {
+            // Add to audit
+            _auditDb.AddLog(new AuditLog
+            {
+                ActorId = new User().FromJson(HttpContext.Session.GetString("user")).Id,
+                IpAddress = HttpContext.Connection.RemoteIpAddress.ToString(),
+                LogType = "logout"
+            });
+
             // Clears session and logs user out.
             HttpContext.Session.Clear();
+
             return RedirectToPage("Index");
         }
     }
