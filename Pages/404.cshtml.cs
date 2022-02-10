4f98335085eb155a6114f7fc3cbdeff7140b9639
Dylan Kok <me@dylankjy.com>
2022-02-11 00:28:18 +0800

    Added 404 page

---
+++ Pages/404.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SITConnect.Pages
+{
+    public class Error404 : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,16 @@ namespace SITConnect
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             // }
+            
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/404";
+                    await next();
+                }
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
